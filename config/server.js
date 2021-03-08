6a48fb72ae04de672a8ae307799c1700a37e19da
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-03-08 08:03:41 -0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36f3e3d2184f287176add73211dd2f3d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7966befc-9561-4f56-a376-3e31127f94ce'
+};
